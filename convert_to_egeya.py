a4cd5f313f93fe7fe879f731cfc01eba593193fd
aborche <zhecka@gmail.com>
2017-01-10 13:14:54 +0300

    added subject translation

--- convert_to_egeya.py
+++ convert_to_egeya.py
@@ -15,8 +15,46 @@ class PostTemplate():
         self.Stamp = datetime.datetime.today()
         self.LastModified = datetime.datetime.today()
         self.Subject = ''
+        self.SubjectTransliterated = ''
         self.Body = ''
         self.isVisible = False
+
+    def transliterate(self,string):
+        ''' cut from https://gist.github.com/aruseni/1685068 '''
+        capital_letters = {u'А':u'A', u'Б':u'B', u'В':u'V', u'Г':u'G', u'Д':u'D', 
+        u'Е':u'E', u'Ё':u'E', u'З':u'Z', u'И':u'I', u'Й':u'Y', u'К':u'K', 
+        u'Л':u'L', u'М':u'M', u'Н':u'N', u'О':u'O', u'П':u'P', u'Р':u'R', 
+        u'С':u'S', u'Т':u'T', u'У':u'U', u'Ф':u'F', u'Х':u'H', u'Ъ':u'', 
+        u'Ы':u'Y', u'Ь':u'', u'Э':u'E',}
+
+        capital_letters_transliterated_to_multiple_letters = {u'Ж':u'Zh', u'Ц':u'Ts', 
+                                                              u'Ч':u'Ch', u'Ш':u'Sh', 
+                                                              u'Щ':u'Sch', u'Ю':u'Yu', 
+                                                              u'Я':u'Ya',}
+
+
+        lower_case_letters = {u'а':u'a', u'б':u'b', u'в':u'v', u'г':u'g', 
+        u'д':u'd', u'е':u'e', u'ё':u'e', u'ж':u'zh', u'з':u'z', u'и':u'i', 
+        u'й':u'y', u'к':u'k', u'л':u'l', u'м':u'm', u'н':u'n', u'о':u'o', 
+        u'п':u'p', u'р':u'r', u'с':u's', u'т':u't', u'у':u'u', u'ф':u'f', 
+        u'х':u'h', u'ц':u'ts', u'ч':u'ch', u'ш':u'sh', u'щ':u'sch', 
+        u'ъ':u'', u'ы':u'y', u'ь':u'', u'э':u'e', u'ю':u'yu', u'я':u'ya',}
+
+        capital_and_lower_case_letter_pairs = {}
+
+        for capital_letter, capital_letter_translit in capital_letters_transliterated_to_multiple_letters.iteritems():
+            for lowercase_letter, lowercase_letter_translit in lower_case_letters.iteritems():
+                capital_and_lower_case_letter_pairs[u"%s%s" % (capital_letter, lowercase_letter)] = u"%s%s" % (capital_letter_translit, lowercase_letter_translit)
+
+        for dictionary in (capital_and_lower_case_letter_pairs, capital_letters, lower_case_letters):
+
+            for cyrillic_string, latin_string in dictionary.iteritems():
+                string = string.replace(cyrillic_string, latin_string)
+
+        for cyrillic_string, latin_string in capital_letters_transliterated_to_multiple_letters.iteritems():
+            string = string.replace(cyrillic_string, latin_string.upper())
+
+        return string
 
 class TagsChanger(HTMLParser):
     def __init__(self):
@@ -89,7 +127,8 @@ class TagsChanger(HTMLParser):
 def ReformatBody(body):
     sbody = TagsChanger()
     sbody.feed(body.replace(u'&nbsp;', ' '))
-    print "="*50,"\n",sbody.get_data()
+    #print "="*50,"\n",sbody.get_data()
+    #print "="*50,"\n",body
     return sbody.get_data()
 
 def ParsePost(post):
@@ -98,20 +137,24 @@ def ParsePost(post):
         print 'PostID: %s'%post['id']
         CurrentPost.Id = post['id']
     if 'date' in post:
-        print 'Post Date %s'%datetime.datetime.strptime(post['date'],'%Y-%m-%d %H:%M:%S')
         CurrentPost.Stamp = datetime.datetime.strptime(post['date'],'%Y-%m-%d %H:%M:%S')
     if 'eventtime' in post:
-        print 'LastModified %s'%datetime.datetime.strptime(post['eventtime'],'%Y-%m-%d %H:%M:%S')
         CurrentPost.LastModified = datetime.datetime.strptime(post['eventtime'],'%Y-%m-%d %H:%M:%S')
     if 'subject' in post:
-        print 'Subject %s'%post['subject']
         CurrentPost.Subject = post['subject']
-#    if 'security' in post:
-#        CurrentPost.isVisible = ReformatBody(post['subject'])
+        CurrentPost.SubjectTransliterated = CurrentPost.transliterate(post['subject']).replace(' ','-')[:64]
+    if 'security' in post:
+        CurrentPost.isVisible = 1 if post['security'] == 'public' else 0
     if 'body' in post:
         CurrentPost.Body = ReformatBody(post['body'])
 #    for item in post:
 #        print item.encode(),'---->',post[item]
+    print 'Date: %s'%CurrentPost.Stamp
+    print 'LastModified: %s'%CurrentPost.LastModified
+    print 'Subject: %s'%CurrentPost.Subject
+    print 'Subject_Trans: %s'%CurrentPost.SubjectTransliterated
+    print 'Body',CurrentPost.Body
+    print "="*50
     
 
 def main():
