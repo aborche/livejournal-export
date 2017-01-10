f300b8ccb1827c55eddb190f3e32f1fd7a3151b8
aborche <zhecka@gmail.com>
2017-01-10 22:47:40 +0300

    add sqlconfig.py

---
+++ sqlconfig.py
@@ -0,0 +1,11 @@
+config = { 
+    'user': 'blogimport', 
+    'password': 'bl0g1234', 
+    'host': 'mysqlblog', 
+    'port': '3306',
+    'database': 'blog',
+#    'client_flags': [ClientFlag.SSL], 
+#    'ssl_ca': '/opt/mysql/ssl/ca.pem', 
+#    'ssl_cert': '/opt/mysql/ssl/client-cert.pem', 
+#    'ssl_key': '/opt/mysql/ssl/client-key.pem', 
+} 
