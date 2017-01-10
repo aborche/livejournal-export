5cc56131b89d65dff54d835b0d1fa92a0932c7ca
aborche <zhecka@gmail.com>
2017-01-10 22:33:32 +0300

    added import to mysql database

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 # copy values of these cookies for LJ from your browser after logging in
 cookies = {
-    'ljloggedin': '',
-    'ljmastersession': '',
+    'ljloggedin': 'v2:u15415003:s57:t1483949289:g6f89a2ee61ee7af37cb4b2839b03a56b6f635a4b',
+    'ljmastersession': 'v2:u15415003:s57:aZdY6JId1Yt:g81e38d0683b7895b5ee13401976e4c0432f6bca3//1',
 }
 
 headers = {
